96e2ed3b35fef3e2e2498e2ff066d3b21272c049
charlieterle <cdieterle33@gmail.com>
2024-08-10 16:11:09 +0900

    Turn CurrentWeather into a card to make it pretty

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -1,28 +1,22 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
 
 export default function CurrentWeather({ weatherData, celFar }) {
   const locationData = weatherData.location;
-  const location = [locationData.name, locationData.region, locationData.country].join(", ")
+  const location = [locationData.name, locationData.region].join(", ");
   const currentData = weatherData.current;
   const currentTemp = celFar === "C" ? Math.round(Number(currentData.temp_c)) : Math.round(Number(currentData.temp_f));
   const weatherStatus = currentData.condition.text;
   return (
-    <Container className="container mt-2">
-      <Row className="">
-        <Col className="bg-primary-subtle col-11 fs-3 text-left">Weather in {location}</Col>
-        <Col className="col-1">
-          <img class="float-right" src={currentData.condition.icon} alt={weatherStatus}></img>
-        </Col>
-      </Row>
-      <Row className="">
-        <Col className="bg-secondary-subtle col-12 fs-2 text-left">{currentTemp}˚{celFar} </Col>
-      </Row>
-      <Row className="">
-        <Col className="bg-secondary-subtle col-12 fs-2 text-left">{weatherStatus}</Col>
-      </Row>
+    <Container className="mt-2">
+      <Card>
+        <Card.Header>{location}</Card.Header>
+        <Card.Body className="">
+          <Card.Img class="float-start" src={currentData.condition.icon} alt={weatherStatus}></Card.Img>
+          <Card.Text className="">{currentTemp}˚{celFar}<br />{weatherStatus}</Card.Text>
+        </Card.Body>
+      </Card>
     </Container>
-  )
+  );
 }
